44e5583a2092df9431f7541687d32cd26573d5ef
mohit199412 <mohitkathuria1994@gmail.com>
2022-07-08 22:44:51 +0530

    update

---
+++ src/main/java/com/bbtutorials/echo/EchoController.java
@@ -0,0 +1,16 @@
+package com.bbtutorials.echo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EchoController {
+	
+	@GetMapping("/echo/{name}")
+    public String echo(@PathVariable String name) {
+	    System.out.println("ARGO CD PIPELINE");
+        return "My ARGO CD PIPELINE" + name;
+    }
+
+}
